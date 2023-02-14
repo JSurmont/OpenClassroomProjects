8ea43290eb9486da8b0ab4380989355778149e94
Julien Surmont <julien.surmont@open-groupe.com>
2023-02-14 18:17:56 +0100

    Ajouter du contenu dans les pages et le dynamiser

--- cours_angular/snapface/src/app/app.component.ts
+++ cours_angular/snapface/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+
+  ngOnInit(): void {
+    this.mySnap = new FaceSnap(
+      'Archibalde',
+      'Mon meilleur ami depuis tout petit !',
+      new Date(),
+      6,
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+    );
+  }
+}

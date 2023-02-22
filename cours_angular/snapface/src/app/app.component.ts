3b3c25cd50baf32de16e8205431c8a2727518002
Julien Surmont <julien.surmont@open-groupe.com>
2023-02-22 22:55:21 +0100

    cours angular : introduction aux subscriptions

--- cours_angular/snapface/src/app/app.component.ts
+++ cours_angular/snapface/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { filter, interval, map, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,19 @@ import { Component, OnInit } from '@angular/core';
 
 export class AppComponent implements OnInit {
 
-  ngOnInit(): void {}
+  interval$!: Observable<string>;
+
+  ngOnInit(): void {
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ?
+        `Je suis ${value} et je suis pair` :
+        `Je suis ${value} et je suis impair`),
+      tap(text => this.logger(text))
+    );
+  }
+
+  logger(text: string) {
+    console.log(`log: ${text}`);
+  }
 }

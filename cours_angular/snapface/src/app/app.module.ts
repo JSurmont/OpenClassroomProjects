2e2a3c1e569634ebf02f24678b67d4d6e9bbdbcf
Julien Surmont <julien.surmont@open-groupe.com>
2023-02-22 01:37:22 +0100

    cours angular : modification de la locale de la date

--- cours_angular/snapface/src/app/app.module.ts
+++ cours_angular/snapface/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 import { HeaderComponent } from './header/header.component';
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr'
 
 @NgModule({
   declarations: [
@@ -16,7 +18,13 @@ import { HeaderComponent } from './header/header.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr-FR' }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(){
+    registerLocaleData(fr.default);
+  }
+}

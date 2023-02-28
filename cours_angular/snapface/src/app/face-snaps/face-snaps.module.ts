6818a32b4134583f11ec5ad4b3bfeda023702b22
Julien Surmont <julien.surmont@open-groupe.com>
2023-02-28 10:32:58 +0100

    cours angular : lazy loading

--- cours_angular/snapface/src/app/face-snaps/face-snaps.module.ts
+++ cours_angular/snapface/src/app/face-snaps/face-snaps.module.ts
@@ -1,11 +1,12 @@
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { FaceSnapListComponent } from './components/face-snap-list/face-snap-list.component';
 import { FaceSnapComponent } from './components/face-snap/face-snap.component';
 import { NewFaceSnapComponent } from './components/new-face-snap/new-face-snap.component';
 import { SingleFaceSnapComponent } from './components/single-face-snap/single-face-snap.component';
+import { FaceSnapsRoutingModule } from './face-snaps-routing.module';
 
 
 
@@ -19,7 +20,7 @@ import { SingleFaceSnapComponent } from './components/single-face-snap/single-fa
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterModule
+    FaceSnapsRoutingModule
   ],
   exports: [
     FaceSnapComponent,

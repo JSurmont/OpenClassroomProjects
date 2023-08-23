dc93063214133e0cb403807daf1596d7b4dc08e5
Julien SURMONT <j.surmont.92@gmail.com>
2023-08-23 16:57:35 +0200

    cours Python : django avancé - ajout du modéle user

--- cours_python/django-advanced/authentication/models.py
+++ cours_python/django-advanced/authentication/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    CREATOR = 'CREATOR'
+    SUBSRIBER = 'SUBSCRIBER'
+
+    ROLE_CHOICES = (
+        (CREATOR, 'Créateur'),
+        (SUBSRIBER, 'Abboné')
+    )
+
+    profile_photo = models.ImageField(verbose_name='Photo de profil')
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Rôle')
+
+

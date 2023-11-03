45b74557fd81e0d26e39ad1a29f1ec42e5486122
Julien SURMONT <j.surmont.92@gmail.com>
2023-11-03 15:11:41 +0100

    cours Python: ajout du projet de decouverte pytest-django

---
+++ cours_python/tester/django-pytest/project_test/library/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils import timezone
+
+class Book(models.Model):
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.author} | {self.title}'

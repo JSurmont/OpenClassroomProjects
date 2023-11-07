8c9dff68a24e060b3a8ae227008a90dee17c7a59
Julien SURMONT <j.surmont.92@gmail.com>
2023-11-07 15:29:44 +0100

    cours Python: paramétrisation des tests dans le projet "logiciel" du cours "testez votre projet"

--- cours_python/tester/calculatrice-projet-logiciel/tests_pytest/test_operators.py
+++ cours_python/tester/calculatrice-projet-logiciel/tests_pytest/test_operators.py
@@ -3,10 +3,11 @@ import pytest
 from calculate.operators import Operators
 
 
-def test_should_make_addition():
+@pytest.mark.parametrize("calculation, result", [("5.5 + 10", 15.5), ("2 + 2 + 10", 14), ("4.8 + 5", 9.8)])
+def test_should_make_addition(calculation, result):
     sut = Operators()
-    operation = "5.5 + 10"
-    expected_value = 15.5
+    operation = calculation
+    expected_value = result
     assert sut.addition(operation) == expected_value
 
 
@@ -17,10 +18,11 @@ def test_should_make_multiple_addition():
     assert sut.addition(operation) == expected_value
 
 
-def test_should_not_make_addition_and_return_none_with_wrong_operator():
+@pytest.mark.parametrize("calculation, result", [("5.5 - 10", None), ("2 + 2 * 10", None)])
+def test_should_not_make_addition_and_return_none_with_wrong_operator(calculation, result):
     sut = Operators()
-    operation = "5.5 * 10"
-    expected_value = None
+    operation = calculation
+    expected_value = result
     assert sut.addition(operation) == expected_value
 
 
@@ -38,10 +40,11 @@ def test_should_not_make_addition_and_return_none_with_non_digit_value():
     assert sut.addition(operation) == expected_value
 
 
-def test_should_make_subtraction():
+@pytest.mark.parametrize("calculation, result", [("5.5 - 10", -4.5), ("10 - 3 - 2", 5), ("5 - 5", 0)])
+def test_should_make_subtraction(calculation, result):
     sut = Operators()
-    operation = "5.5 - 10"
-    expected_value = -4.5
+    operation = calculation
+    expected_value = result
     assert sut.subtraction(operation) == expected_value
 
 
@@ -52,10 +55,11 @@ def test_should_make_multiple_subtraction():
     assert sut.subtraction(operation) == expected_value
 
 
-def test_should_not_make_subtraction_and_return_none_with_wrong_operator():
+@pytest.mark.parametrize("calculation, result", [("5.5 + 10", None), ("2 - 2 * 10", None)])
+def test_should_not_make_subtraction_and_return_none_with_wrong_operator(calculation, result):
     sut = Operators()
-    operation = "5.5 * 10"
-    expected_value = None
+    operation = calculation
+    expected_value = result
     assert sut.subtraction(operation) == expected_value
 
 
@@ -73,10 +77,11 @@ def test_should_not_make_subtraction_and_return_none_with_non_digit_value():
     assert sut.subtraction(operation) == expected_value
 
 
-def test_should_make_multiplication():
+@pytest.mark.parametrize("calculation, result", [("5.5 * 10", 55), ("100 * 2 * 4", 800), ("4.8 * 5", 24)])
+def test_should_make_multiplication(calculation, result):
     sut = Operators()
-    operation = "5.5 * 10"
-    expected_value = 55.0
+    operation = calculation
+    expected_value = result
     assert sut.multiplication(operation) == expected_value
 
 
@@ -87,10 +92,11 @@ def test_should_make_multiple_multiplication():
     assert sut.multiplication(operation) == expected_value
 
 
-def test_should_not_make_multiplication_and_return_none_with_wrong_operator():
+@pytest.mark.parametrize("calculation, result", [("5.5 + 10", None), ("2 * 2 / 10", None)])
+def test_should_not_make_multiplication_and_return_none_with_wrong_operator(calculation, result):
     sut = Operators()
-    operation = "5.5 + 10"
-    expected_value = None
+    operation = calculation
+    expected_value = result
     assert sut.multiplication(operation) == expected_value
 
 
@@ -108,10 +114,11 @@ def test_should_not_make_multiplication_and_return_none_with_non_digit_value():
     assert sut.multiplication(operation) == expected_value
 
 
-def test_should_make_division():
+@pytest.mark.parametrize("calculation, result", [("5.5 / 10", 0.55), ("100 / 2 / 10", 5), ("5 / 5", 1)])
+def test_should_make_division(calculation, result):
     sut = Operators()
-    operation = "5.5 / 10"
-    expected_value = 0.55
+    operation = calculation
+    expected_value = result
     assert sut.division(operation) == expected_value
 
 
@@ -129,10 +136,11 @@ def test_should_not_make_division_and_return_none_with_denominator_zero():
     assert sut.division(operation) == expected_value
 
 
-def test_should_not_make_division_and_return_none_with_wrong_operator():
+@pytest.mark.parametrize("calculation, result", [("5.5 + 10", None), ("2 / 2 + 10", None)])
+def test_should_not_make_division_and_return_none_with_wrong_operator(calculation, result):
     sut = Operators()
-    operation = "5.5 + 10"
-    expected_value = None
+    operation = calculation
+    expected_value = result
     assert sut.division(operation) == expected_value
 
 

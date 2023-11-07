9dab53ee8044e1ab7de012a14bc96c53c162fbdb
Julien SURMONT <j.surmont.92@gmail.com>
2023-11-07 17:07:55 +0100

    cours Python: Organisons en classes de test (sans fixture) pour les TU du "site ecommerce" Django

---
+++ cours_python/tester/site-ecommerce-Django/tests_classes/home/test_views.py
@@ -0,0 +1,29 @@
+from django.urls import reverse
+from django.test import Client
+from pytest_django.asserts import assertTemplateUsed
+import pytest
+
+
+class TestHomeViews:
+    client = Client()
+
+    def test_redirect_home_view(self):
+        """
+        Testing if our RedirectHomeView redirects successfully (status_code 302)
+        For the second assert, We are testing if we redirect to the '/home/' url
+         """
+        response = self.client.get(reverse('redirect_home'))
+
+        assert response.status_code == 302
+        assert response.url == '/home/'
+
+    @pytest.mark.django_db
+    def test_HomeView(self):
+        """
+        In the first assert, We are testing if our get request returns 200 (OK) status code
+        For the second assert, we are making sure that our view returns the home.html template
+        """
+        response = self.client.get(reverse('home'))
+
+        assert response.status_code == 200
+        assertTemplateUsed(response, 'home.html')
